3d4b5cbaef6db7039a0d7e62ab723af2ebf9dd3a
stephangriesel <sgriesel@gmail.com>
2019-03-23 11:40:30 +0100

    add owner property

--- models/comment-model.js
+++ models/comment-model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
-const taskSchema = new Schema({
+const commentSchema = new Schema({
   title: String,
   description: String,
   topic: {type: Schema.Types.ObjectId, ref: 'Topic'}
 });
 
-const Comment = mongoose.model('Comment', taskSchema);
+const Comment = mongoose.model('Comment', commentSchema);
 
 module.exports = Comment;
--- models/topic-model.js
+++ models/topic-model.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
+const User = require('./user-model');
 
 const topicSchema = new Schema({
   title: String,
   description: String,
-  tasks: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
-  // owner will be added later on
+  comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
+  owner: {type: Schema.Types.ObjectId, ref: 'User'}
 });
 
 const Topic = mongoose.model('Topic', topicSchema);
